c128aee774118194a580e66be3295e31c7e3ff63
Jose Luiz <jlsystem-learn@outlook.com>
2021-07-27 00:39:01 -0300

    Atualização dos arquivos da pasta 'browser' e criação do arquivo de estilo 'styles.css

--- browser/ppt.js
+++ browser/ppt.js
@@ -1,3 +1,4 @@
+/*-----------------------------------------------------(Código Antigo 23/07/2021)------------------------------------------------------------------ 
 let scoreJogador = 0;
 let scoreComputador = 0;
 const botoes = document.querySelectorAll('input');
@@ -54,4 +55,148 @@ function executarRound(selecaoJogador)
 }
 
 botoes.forEach(botao => {botao.addEventListener('click', function(){executarRound(botao.value)})});
+--------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+let scoreJogador = 0;
+let scoreComputador = 0;
+let paraCabecalhoPontuacao = "";
+
+const simboloBotoes = document.querySelectorAll(".btn");
+simboloBotoes.forEach((button) => button.addEventListener("click", jogarRound));
+
+// Inicia toda a logica de jogo e apresenta mensagem final de jogo.
+function jogarRound(event) 
+{
+    let botaoId;
+
+    if(jogoFinalizado())
+    {
+        alert("Fim do jogo! Aperte F5 para jogar novamente.");
+        return;
+    }
+
+    if(event.target.tagName.toLowerCase() === "i")
+    {
+        // Pega o string do id do nó pai (button)
+        botaoId = event.target.parentNode.id;
+        //converterSimboloIdMaiusculo(botaoId);
+        //alert("O id do botão clicado é: " + botaoId);
+    }
+    else
+    {
+        botaoId = event.target.id;
+    }
+
+    const selecaoJogador = converterSimboloIdMaiusculo(botaoId);
+    const selecaoComputador = pegarEscolhaRandomica();
+    atualizarPontucao(obterVencedor(selecaoJogador, selecaoComputador));
+    atualizarEscolhas(selecaoJogador, selecaoComputador);
+}
+
+// Retorna vitória da seleção do Jogador, do Computador ou empate.
+function obterVencedor(selecaoJogador, selecaoComputador)
+{
+    if(selecaoJogador === selecaoComputador)
+    {
+        return "empatado"
+    }
+
+    if(selecaoJogador === "ROCK" && selecaoComputador === "SCISSORS" || (selecaoJogador === "SCISSORS" && selecaoComputador ==="PAPER" || (selecaoJogador === "PAPER" && selecaoComputador === "ROCK")))
+    {
+        return "jogador";
+    }
+
+    if(selecaoComputador === "ROCK" && selecaoJogador === "SCISSORS" || (selecaoComputador === "SCISSORS" && selecaoJogador ==="PAPER" || (selecaoComputador === "PAPER" && selecaoJogador === "ROCK")))
+    {
+        return "computador";
+    }
+}
+
+// Atualiza as escolhas do Jogador e do Computador e as exibe nos elementos de HTML.
+function atualizarEscolhas(selecaoJogador, selecaoComputador)
+{
+    const simboloJogador = document.getElementById("simbolo-jogador");
+    const simboloComputador = document.getElementById("simbolo-computador");
+
+    simboloJogador.classList.add("active");
+    simboloComputador.classList.add("active");
+
+    const nomeClasseSimboloJogador = `fa-hand-${selecaoJogador.toLowerCase()}`;
+    console.log(nomeClasseSimboloJogador);
+    const nomeClasseSimboloComputador = `fa-hand-${selecaoComputador.toLowerCase()}`;
+    console.log(nomeClasseSimboloComputador);
+
+    simboloJogador.classList = `fas ${nomeClasseSimboloJogador} active`;
+    simboloComputador.classList = `fas ${nomeClasseSimboloComputador} active`;
+}
+
+// Atualiza pontuação no elemento h2 de HTML.
+function atualizarPontucao(vencedor)
+{
+    paraCabecalhoPontuacao = document.getElementById("pontuacao-cabecalho");
+    const paraPontuacaoJogador = document.getElementById("placar-jogador");
+    const paraPontuacaoComputador = document.getElementById("placar-computador");
+
+    if(vencedor === "empatado")
+    {
+        paraCabecalhoPontuacao.textContent = "Deu empate!";
+    }
+    else if(vencedor === "jogador")
+    {
+        paraCabecalhoPontuacao.textContent = "Você venceu!";
+        scoreJogador++;
+    }
+    else if(vencedor === "computador")
+    {
+        paraCabecalhoPontuacao.textContent = "Você perdeu!";
+        scoreComputador++;
+    }
+
+    paraPontuacaoJogador.textContent = `Jogador: ${scoreJogador}`;
+    paraPontuacaoComputador.textContent = `Computador: ${scoreComputador}`;
+
+    if(jogoFinalizado())
+    {
+        definirMensagemFinal();
+    }
+}
+
+// Define mensagem final para o fim de jogo.
+function definirMensagemFinal()
+{
+    return scoreJogador > scoreComputador 
+    ? (paraCabecalhoPontuacao.textContent = "GameOver, Você Venceu!") 
+    : (paraCabecalhoPontuacao.textContent = "GameOver, Você Perdeu!");
+}
+
+// Converte string do id do elemento HTML em minúsculo para maiúsculo.
+function converterSimboloIdMaiusculo(botaoId)
+{
+    // Pega o primeiro indice '0' do Id do botão separado por '-' e transforma em Maiúsculo.
+    //let stringMaiusculo = botaoId.split("-")[0].toUpperCase();
+    //alert("String do id do botão para maiúsculo: " + stringMaiusculo);
+    return botaoId.split("-")[0].toUpperCase();
+}
+
+function jogoFinalizado()
+{
+    return scoreJogador === 5 || scoreComputador === 5;
+}
+
+// Executa uma escolha randômica e a exibe como escolha do Computador.
+function pegarEscolhaRandomica() 
+{
+    let numeroRandomico = Math.floor(Math.random() * simboloBotoes.length);
+
+    switch(numeroRandomico)
+    {
+        case 0:
+            return "ROCK";
+        case 1:
+            return "PAPER";
+        case 2:
+            return "SCISSORS";
+    }
+}
+
 
